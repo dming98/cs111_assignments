f56e86ae32f290357f0b0f523eb0a20ffae8f9df
dming98 <drminger98@gmail.com>
2017-01-17 15:48:30 -0500

    ERDataReader Final

--- assignment2/src/er_data/ERDataReader.java
+++ assignment2/src/er_data/ERDataReader.java
@@ -6,8 +6,22 @@ import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 
 public class ERDataReader {
-	public static int[][][] readData(String dataFile)
-			throws FileNotFoundException, NoSuchElementException, IllegalStateException  {	
-
+	public static int[][][] readData(String dataFile) throws FileNotFoundException, NoSuchElementException, IllegalStateException  {	
+		Scanner file = new Scanner(new FileReader(dataFile));
+		
+		final int NUM_WEEKS = 4;
+		final int NUM_DAYS = 7;
+		final int NUM_HOURS = 24;
+		
+		int[][][] data = new int[NUM_WEEKS][NUM_DAYS][NUM_HOURS];
+		
+		for(int h=0;h<NUM_WEEKS;h++){
+			for(int j=0;j<NUM_DAYS;j++){
+				for(int k=0;k<NUM_HOURS;k++){
+						data[h][j][k] = file.nextInt();
+				}
+			}
+		}
+		return data;
 	}
 }
